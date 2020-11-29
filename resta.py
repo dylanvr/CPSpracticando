886e43cbfe221442b6304381a0ccacbbc6d25f4c
dylanvr <dylanvr97@gmail.com>
2020-11-29 11:20:27 -0500

    se documentan la funcionalidades

--- resta.py
+++ resta.py
@@ -1,2 +1,14 @@
-def resta (x,y):
-    return (x-y)
+def resta (numeroa,numerob):
+    """funcion resta dos numeros numeroa-numerob
+    Parameters
+    ----------
+    parametro_1 : tipo real 
+        un numero real 
+    parametro_2 : tipo real
+        un numero real
+    Returns
+    -------
+    tipo real
+        devuelve la resta de los dos numeros 
+    """
+    return numeroa-numerob
--- suma.py
+++ suma.py
@@ -1,3 +1,15 @@
-def suma (x,y):
-    return x+y
+def suma (numeroa,numerob):
+    """funcion suma de dos numeros numeroa-numerob
+    Parameters
+    ----------
+    parametro_1 : tipo real 
+        un numero real 
+    parametro_2 : tipo real
+        un numero real
+    Returns
+    -------
+    tipo real
+        devuelve la suma de los dos numeros 
+    """
+    return numeroa+numerob
 
